9ad9bfaf0b6e706c37231844c717c4a2fbfc040b
Sattwik Biswal <sattwyk@gmail.com>
2026-09-02 16:27:29 +0530

    implemented running with two modes based on the flag

---
+++ LOG.md
@@ -0,0 +1,16 @@
+# Task 1
+
+To setup the cli to read the arguments and run it on two different modes
+
+it needs two flags
+- primary / p
+- replica / r
+
+rkv --primary
+rkv --replica
+rkv -p
+rkv -r
+
+if no flag received it will default to primary
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::env;
+
+enum Mode {
+    Primary,
+    Replica,
+}
+
+fn run(mode: Mode) {
+    match mode {
+        Mode::Primary => println!("running as a primary node"),
+        Mode::Replica => println!("running as a replica node"),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() > 2 {
+        eprintln!("Please only use between --primary , --replica, -p or -r");
+        return;
+    }
+
+    if args.len() == 1 {
+        run(Mode::Primary);
+        return;
+    }
+
+    let flag = &args[1];
+
+    match flag.as_str() {
+        "-p" => run(Mode::Primary),
+        "-r" => run(Mode::Replica),
+        "--primary" => run(Mode::Primary),
+        "--replica" => run(Mode::Replica),
+        _ => eprintln!("Please only use between --primary , --replica, -p or -r"),
+    }
 }
